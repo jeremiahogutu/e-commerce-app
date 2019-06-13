d093f0764f30f0c5f903a4421e09f5638bcf421c
Jeremiah Ogutu <jogutu@pm.me>
2019-06-12 20:37:46 -0400

    connecting to mongo db

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+// app
+const app = express();
+
+
+// database
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log('DB Connected')
+});
+
+// routes
 app.get('/', (req, res) => {
     res.send('hello from node')
 });
