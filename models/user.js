d3b5e00c8c7b1e25eb64a038a26979889a339309
Jeremiah Ogutu <jogutu@pm.me>
2019-06-12 21:16:11 -0400

    virtual fields and methods

--- models/user.js
+++ models/user.js
@@ -35,3 +35,30 @@ const userSchema = new mongoose.Schema({
         default: []
     }
 }, {timestamps: true});
+
+// virtual field
+userSchema.virtual('password');
+.set(function (password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.hashed_password = this.encryptPassword(password)
+})
+.get(function () {
+    return this._password
+});
+
+userSchema.methods = {
+    encryptPassword: function (password) {
+        if (!password) return '';
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex')
+        } catch (err) {
+            return ''
+        }
+    }
+};
+
+module.exports = mongoose.model('User', userSchema);
