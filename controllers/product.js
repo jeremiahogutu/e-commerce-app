efc5d5403e6e93158ce2c194f80ef4d1abbb1a07
Jeremiah Ogutu <jogutu@pm.me>
2019-06-14 00:19:17 -0400

    product by id middleware

--- controllers/product.js
+++ controllers/product.js
@@ -4,6 +4,23 @@ const fs = require('fs');
 const Product = require('../models/product');
 const {errorHandler} = require('../helpers/dbErrorHandler');
 
+exports.productById = (req, res, next, id) => {
+    Product.findById(id).exec((err, product) => {
+        if (err || !product) {
+            return res.status(400).json({
+                error: "Image could not be uploaded"
+            })
+        }
+        req.product = product;
+        next()
+    })
+};
+
+exports.read = (req, res) => {
+    req.product.photo = undefined;
+    return res.json(req.product)
+}
+
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {create} = require('../controllers/product');
+const {create, productById, read} = require('../controllers/product');
 const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 const {userById} = require("../controllers/user");
 
+router.get('/product/:productId', read)
 router.post(
     '/product/create/:userId',
     requireSignin,
@@ -14,5 +15,6 @@ router.post(
 );
 
 router.param('userId', userById);
+router.param('productId', productById);
 
 module.exports = router;
