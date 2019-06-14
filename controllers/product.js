340acd700e7d192b1bedf910b036217afb46e593
Jeremiah Ogutu <jogutu@pm.me>
2019-06-13 23:04:32 -0400

    create product validation

--- controllers/product.js
+++ controllers/product.js
@@ -13,9 +13,42 @@ exports.create = (req, res) => {
                 error: 'Image could not be uploaded'
             })
         }
+
+        // check for all fields
+        const {
+            name,
+            description,
+            price,
+            category,
+            quantity,
+            shipping
+        } = fields;
+
+        if (
+            !name ||
+            !description ||
+            !price ||
+            !category ||
+            !quantity ||
+            !shipping
+        ) {
+            return res.status(400).json({
+                error: "All fields are required"
+            })
+        }
+
         let product = new Product(fields);
 
+        // 1kb = 1000
+        // 1mb = 1000000
         if (files.photo) {
+            // console.log('FILES PHOTO', files);
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image should be less than 1mb in size"
+                })
+            }
+
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type
         }
