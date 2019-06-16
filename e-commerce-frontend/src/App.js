541f0d79544d4dcca7e159a8416ca014802bd3d7
Jeremiah Ogutu <jogutu@pm.me>
2019-06-16 18:25:12 -0400

    react init

---
+++ e-commerce-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+      <div className='container'>
+          <div className='notification has-background-white'>
+              <h2>Hello from React</h2>
+          </div>
+      </div>
+  );
+};
+
+
+export default App;
