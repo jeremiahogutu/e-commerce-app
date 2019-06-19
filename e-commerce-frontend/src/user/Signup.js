ee965ff062d8b78144d89d0a8f0e36f501ff6b04
Jeremiah Ogutu <jogutu@pm.me>
2019-06-19 08:10:08 -0400

    signup validation

--- e-commerce-frontend/src/user/Signup.js
+++ e-commerce-frontend/src/user/Signup.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import Layout from "../main/Layout";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faLock, faUser, faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import {faLock, faUser, faEnvelope, faExclamationTriangle} from "@fortawesome/free-solid-svg-icons";
 import {API} from "../config";
 
 const Signup = () => {
@@ -13,14 +13,21 @@ const Signup = () => {
         success: false
     });
 
-    const {name, email, password} = values;
+    const {name, email, password, success, error} = values;
 
     const handleChange = userInput => event => {
-        setValues({...values, error: false, [userInput]: event.target.value })
+        setValues({
+            ...values,
+            error: false,
+            [userInput]: event.target.value
+        });
+
+        document.getElementById('userNameError').style.visibility = 'hidden';
+        document.getElementById('userName').classList.remove('is-danger')
     };
 
     const signup = user => {
-        fetch(`${API}/signup`, {
+        return fetch(`${API}/signup`, {
             method: "POST",
             headers: {
                 Accept: 'application/json',
@@ -36,40 +43,123 @@ const Signup = () => {
             })
     };
 
+    const formValidator = () => {
+        const userName = document.getElementById('userName').value;
+        const userEmail = document.getElementById('userEmail').value;
+        const userPassword = document.getElementById('userPassword').value;
+        if (userName === "") {
+            alert("Name must be filled out");
+            return false;
+        } else if (userEmail === "") {
+            alert("Email must be filled out");
+            return false;
+        } else if (userPassword === "") {
+            alert("Password must be filled out");
+            return false;
+        }
+    };
+
     const clickSubmit = event => {
         event.preventDefault();
         signup({name, email, password})
+            .then(data => {
+                if (data.error) {
+                    setValues({
+                        ...values,
+                        error: data.error,
+                        success: false
+                    })
+                } else {
+                    setValues({
+                        ...values,
+                        name: '',
+                        email: '',
+                        password: '',
+                        error: '',
+                        success: true
+                    })
+                }
+            })
+    };
+
+    const showError = () => {
+        if (error === "Name is required") {
+            document.getElementById('userNameError').style.visibility = 'visible';
+            document.getElementById('userName').classList.add('is-danger')
+        }
+        return (
+
+            <div className="is-danger" style={{display: error ? '' : 'none'}}>
+                {error}
+            </div>
+        )
+    };
+
+    const showSuccess = () => {
+        return (
+            <div className="is-danger" style={{display: success ? '' : 'none'}}>
+                {success}
+            </div>
+        )
     };
 
 
-
     const signUpForm = () => (
-        <form style={{maxWidth: '900px'}}>
+        <form name="myForm" style={{maxWidth: '900px'}}>
             <div className="field">
                 <label className="label">Name</label>
-                <p className="control is-expanded has-icons-left">
-                    <input onChange={handleChange('name')} className="input" type="text" placeholder="Name"/>
+                <p className="control is-expanded has-icons-left has-icons-right">
+                    <input
+                        id='userName'
+                        onChange={handleChange('name')}
+                        className="input"
+                        type="text"
+                        placeholder="Name"
+                        value={name}
+                    />
                     <span className="icon is-small is-left">
           <FontAwesomeIcon icon={faUser}/>
         </span>
+                    <span id="userNameError" className="icon is-small is-right" style={{visibility: 'hidden'}}>
+      <FontAwesomeIcon icon={faExclamationTriangle}/>
+    </span>
                 </p>
             </div>
+            <p className="help is-danger">{error}</p>
             <div className="field">
                 <label className="label">Email</label>
                 <p className="control has-icons-left has-icons-right">
-                    <input onChange={handleChange('email')} className="input" type="email" placeholder="Email"/>
+                    <input
+                        id="userEmail"
+                        onChange={handleChange('email')}
+                        className="input is-danger"
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                    />
                     <span className="icon is-small is-left">
       <FontAwesomeIcon icon={faEnvelope}/>
     </span>
+                    {/*                <span className="icon is-small is-right">*/}
+                    {/*  <i className="fas fa-check"/>*/}
+                    {/*</span>*/}
                     <span className="icon is-small is-right">
-      <i className="fas fa-check"/>
+      <FontAwesomeIcon icon={faExclamationTriangle}/>
     </span>
                 </p>
             </div>
+            <p className="help is-danger">This email is invalid</p>
             <div className="field">
                 <label className="label">Password</label>
                 <p className="control has-icons-left">
-                    <input onChange={handleChange('password')} className="input" type="password" placeholder="Password"/>
+                    <input
+                        id="userPassword"
+                        onChange={handleChange('password')}
+                        className="input"
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                    />
                     <span className="icon is-small is-left">
       <FontAwesomeIcon icon={faLock}/>
     </span>
@@ -92,6 +182,8 @@ const Signup = () => {
             className="container is-fluid is-flex is-centered"
         >
             <div className="notification has-background-white">
+                {showSuccess()}
+                {showError()}
                 {signUpForm()}
                 {JSON.stringify(values)}
             </div>
