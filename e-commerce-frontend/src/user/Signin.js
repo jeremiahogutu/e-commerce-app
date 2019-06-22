4ed1c3fbf27aa48ca68372d98a3050af6fb0afb0
Jeremiah Ogutu <jogutu@pm.me>
2019-06-22 16:26:06 -0400

    user and admin dashboard

--- e-commerce-frontend/src/user/Signin.js
+++ e-commerce-frontend/src/user/Signin.js
@@ -92,6 +92,9 @@ const Signin = () => {
                 return <Redirect to="/user/dashboard"/>
             }
         }
+        if (isAuthenticated()) {
+            return <Redirect to="/"/>
+        }
     };
 
     const signInForm = () => (
