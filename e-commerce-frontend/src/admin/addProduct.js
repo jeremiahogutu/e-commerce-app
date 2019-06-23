ba804aa1bbd81a99db622bac9ccc81e6a74da056
Jeremiah Ogutu <jogutu@pm.me>
2019-06-22 23:17:38 -0400

    create product part 2

--- e-commerce-frontend/src/admin/addProduct.js
+++ e-commerce-frontend/src/admin/addProduct.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {isAuthenticated} from "../auth";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCloudUploadAlt, faPlusSquare} from "@fortawesome/free-solid-svg-icons";
+import {faCloudUploadAlt, faPlusSquare, faDollarSign} from "@fortawesome/free-solid-svg-icons";
 import Layout from "../main/Layout";
 import {createProduct} from "./apiAdmin";
 import {NavLink} from "react-router-dom";
@@ -41,26 +41,41 @@ const AddProduct = () => {
         formData
     } = values;
 
-    const clickSubmit = () => {
+    useEffect(() => {
+        setValues({
+            ...values,
+            formData: new FormData()
+        })
+    }, []);
+
+    const clickSubmit = (e) => {
 
     };
 
-    const handleChange = () => {
+    const handleChange = name => event => {
+        const value = name === 'photo' ? event.target.files[0] : event.target.value;
+        formData.set(name, value);
+        if (name === 'photo') {
+            const productPhoto = document.getElementById('productPhoto');
+            if (productPhoto.files && productPhoto.files[0]) {
+                const reader = new FileReader();
 
-    };
+                reader.onload = function (e) {
+                    document.getElementById('productPhotoPreview').src = e.target.result;
+                };
 
-    const readURL = () => {
-        const productPhoto = document.getElementById('productPhoto');
-        if (productPhoto.files && productPhoto.files[0]) {
-            const reader = new FileReader();
-
-            reader.onload = function (e) {
-                document.getElementById('productPhotoPreview').src = e.target.result;
-            };
-
-            reader.readAsDataURL(productPhoto.files[0]);
+                reader.readAsDataURL(productPhoto.files[0]);
+            }
         }
+        setValues({
+            ...values,
+            [name]: value
+        })
     };
+
+    // const readURL = (photo) => {
+    //
+    // };
 
     const newPostForm = () => (
         <form onSubmit={clickSubmit} style={{maxWidth: '900px'}}>
@@ -75,9 +90,8 @@ const AddProduct = () => {
                             type="file"
                             id="productPhoto"
                             name="photo"
-                            onChange={() => {
-                                readURL()
-                            }}
+                            accept="image/*"
+                            onChange={handleChange('photo')}
                         />
                         <span className="file-cta">
         <span className="file-icon">
@@ -95,17 +109,76 @@ const AddProduct = () => {
                 <p className="control has-icons-left has-icons-right">
                     <input
                         className="input"
-                        onChange={handleChange}
+                        onChange={handleChange('name')}
                         type="text"
-                        placeholder="New Product"
+                        placeholder="Product Name"
                         value={name}
                         autoFocus
-                        required
                     />
                     <span className="icon is-small is-left">
       <FontAwesomeIcon icon={faPlusSquare}/>
     </span>
                 </p>
+            </div>
+            <div className="field" style={{marginTop: '25px'}}>
+                <p className="control">
+                    <input
+                        className="textarea"
+                        onChange={handleChange('description')}
+                        placeholder="Description"
+                        value={description}
+                    />
+                </p>
+            </div>
+            <div className="field" style={{marginTop: '25px'}}>
+                <p className="control has-icons-left">
+                    <input
+                        className="input"
+                        onChange={handleChange('price')}
+                        type="number"
+                        placeholder="Price"
+                        value={price}
+                    />
+                    <span className="icon is-small is-left">
+      <FontAwesomeIcon icon={faDollarSign}/>
+    </span>
+                </p>
+            </div>
+            <div className="field">
+                <label className="label">Category</label>
+                <div className="control">
+                    <div className="select">
+                        <select onChange={handleChange('category')}>
+                            <option value="5d02e6a455092e25d7faf084">Node</option>
+                            <option value="5d058cd663b50a39855e090b">Python</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div className="field" style={{marginTop: '25px'}}>
+                <p className="control has-icons-left">
+                    <input
+                        className="input"
+                        onChange={handleChange('quantity')}
+                        type="number"
+                        placeholder="Quantity"
+                        value={quantity}
+                    />
+                    <span className="icon is-small is-left">
+      <FontAwesomeIcon icon={faPlusSquare}/>
+    </span>
+                </p>
+            </div>
+            <div className="field">
+                <label className="label">Shipping</label>
+                <div className="control">
+                    <div className="select">
+                        <select onChange={handleChange('shipping')}>
+                            <option value="0">No</option>
+                            <option value="1">Yes</option>
+                        </select>
+                    </div>
+                </div>
             </div>
             <div className="field" style={{marginTop: '25px'}}>
                 <p className="control">
