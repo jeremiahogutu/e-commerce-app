e0cd9ea332387e044cd07988eaeb994057e1c749
Jeremiah Ogutu <jogutu@pm.me>
2019-06-26 00:11:24 -0400

    search form

--- e-commerce-frontend/src/main/Search.js
+++ e-commerce-frontend/src/main/Search.js
@@ -10,7 +10,7 @@ const Search = () => {
         searched: false
     });
 
-    const { categories, category, search, results, searched } = data;
+    const {categories, category, search, results, searched} = data;
 
     const loadCategories = () => {
         getCategories().then(data => {
@@ -25,11 +25,56 @@ const Search = () => {
     useEffect(() => {
         loadCategories()
     }, []);
-    
+
+    const searchSubmit = () => {
+
+    };
+
+    const handleChange = () => {
+
+    };
+
+    const searchForm = () => (
+        <form onSubmit={searchSubmit}>
+            <div className="field is-grouped" style={{flexWrap: 'wrap'}}>
+                <div className="field is-horizontal">
+                    <div className="field-body">
+                        <div className="field is-expanded">
+                            <div className="field has-addons">
+                                <p className="control">
+                                    <div className="select is-fullwidth">
+                                        <select onChange={handleChange("category")}>
+                                            <option value="All">Pick Category</option>
+                                            {categories.map((category, i) => (<option key={i} value={category._id}>{category.name}</option>))}
+                                            <option>Sales</option>
+                                        </select>
+                                    </div>
+                                </p>
+                                <p className="control is-expanded">
+                                    <input
+                                        className="input"
+                                        type="text"
+                                        placeholder="search by name"
+                                        onChange={handleChange('search')}
+                                    />
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p className="control" style={{marginLeft: '10px'}}>
+                    <button className="button is-primary">
+                        Search
+                    </button>
+                </p>
+            </div>
+        </form>
+    );
+
     return (
         <div className="column">
-            <div className="column is-11-desktop is-offset-1-desktop">
-                <h3 className="is-size-4 has-text-weight-bold has-text-black">Search Bar {JSON.stringify(categories)}</h3>
+            <div className="column is-11-desktop is-offset-1-desktop is-flex" style={{justifyContent: 'center'}}>
+                <h3 className="is-size-4 has-text-weight-bold has-text-black">{searchForm()}</h3>
             </div>
         </div>
     );
