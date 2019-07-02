cf439942a5509f2ee1c2648c00820ac4a7fe9d7a
Jeremiah Ogutu <jogutu@pm.me>
2019-07-01 23:32:33 -0400

    loop through your orders

--- e-commerce-frontend/src/admin/Orders.js
+++ e-commerce-frontend/src/admin/Orders.js
@@ -2,11 +2,12 @@ import React, {useState, useEffect} from 'react';
 import {isAuthenticated} from "../auth";
 import Layout from "../main/Layout";
 import {listOrders} from "./apiAdmin";
+import moment from 'moment'
 
 const Orders = () => {
     const [orders, setOrders] = useState([]);
 
-    const {user, token} = isAuthenticated()
+    const {user, token} = isAuthenticated();
 
     const loadOrders = () => {
         listOrders(user._id, token).then(data => {
@@ -20,22 +21,55 @@ const Orders = () => {
 
     useEffect(() => {
         loadOrders()
-    }, [])
+    }, []);
 
-    const noOrders = orders => {
-        return orders.length < 1 ? <h4>No orders</h4> : null;
-
-    }
+    const showOrdersLength = () => {
+        if (orders.length > 0) {
+            return (
+                <h1 className='is-size-4 has-text-weight-bold has-text-black'>Total orders: {orders.length}</h1>
+            )
+        } else {
+            return <h1 className='has-text-danger'>No orders</h1>
+        }
+    };
 
     return (
         <Layout
             title="Orders"
-            description={`Hey ${user.name}, you can manage all the order here!`}
-            className="is-flex"
+            description={`Hey ${user.name}, you can manage all the orders here!`}
         >
-            <div className="notification has-background-white">
-                {noOrders(orders)}
-                {JSON.stringify(orders)}
+            <div style={{padding: '20px 15px', maxWidth: '600px', margin: '0 auto'}}>
+                {showOrdersLength()}
+                {orders.map((order, orderIndex) => {
+                    return (
+                        <div key={orderIndex} style={{margin: '25px 0'}}>
+                            <h2 className="is-size-4 has-text-weight-bold has-text-black">Order ID: {order._id}</h2>
+                            <div className="list">
+                                <li className="list-item has-background-white">
+                                    Status: <span className="has-text-info">{order.status}</span>
+                                </li>
+                                <li className="list-item">
+                                    Transaction ID: <span className="has-text-info">{order.transaction_id}</span>
+                                </li>
+                                <li className="list-item">
+                                    Amount: <span className="has-text-info">${order.amount}</span>
+                                </li>
+                                <li className="list-item is-capitalized">
+                                    Ordered By: <span className="has-text-info">{order.user.name}</span>
+                                </li>
+                                <li className="list-item">
+                                    Ordered On: <span className="has-text-info">{moment(order.createdAt).fromNow()}</span>
+                                </li>
+                                <li className="list-item">
+                                    Delivery Address: <span className="has-text-info">{order.address}</span>
+                                </li>
+                                <li className="list-item is-capitalized">
+                                    Total products ordered: <span className="has-text-info">{order.products.length}</span>
+                                </li>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         </Layout>
     );
