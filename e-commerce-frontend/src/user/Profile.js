436c4ef72c1140ba96ee4fc9989f9e3a0b4533a4
Jeremiah Ogutu <jogutu@pm.me>
2019-07-02 08:17:54 -0400

    get user info for profile update

--- e-commerce-frontend/src/user/Profile.js
+++ e-commerce-frontend/src/user/Profile.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from "../main/Layout";
 import {isAuthenticated} from "../auth";
 import {Link} from "react-router-dom";
+import {read, update, updateUser} from "./ApiUser";
 
-const Profile = () => {
+const Profile = ({match}) => {
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: '',
+        success: false
+    });
+
+    const {token} = isAuthenticated()
+
+    const {name, email, password, error, success} = values;
+
+    const init = (userId) => {
+        read(userId, token).then(data => {
+            if (data.error) {
+                setValues({
+                    ...values,
+                    error: true
+                })
+            } else {
+                setValues({
+                    ...values,
+                    name: data.name,
+                    email: data.email
+                })
+            }
+        })
+    };
+
+    useEffect(() => {
+        init(match.params.userId)
+    });
+
     return (
-        <h2>Profile</h2>
-    );
+        <Layout
+            title="Profile"
+            description="Update your profile"
+        >
+            <h2>Profile Update</h2>
+            {JSON.stringify(values)}
+        </Layout>
+    )
 };
 
 export default Profile;
